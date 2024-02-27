4bdcdd8c60681426899fdab7e0f6b5bad73bf20f
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2024-02-27 22:53:03 +0100

    Change rules in php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,6 +8,11 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'phpdoc_annotation_without_dot' => false,
+        'phpdoc_summary' => false,
+        'yoda_style' => true,
+        'single_trait_insert_per_statement' => false,
     ])
     ->setFinder($finder)
 ;
