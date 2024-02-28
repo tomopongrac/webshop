dcb027f1876631017d291f2a67b9a59893829f72
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2024-02-28 18:36:17 +0100

    Install symfony security

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     TomoPongrac\WebshopApiBundle\WebshopApiBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
