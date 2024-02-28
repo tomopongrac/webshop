34649cbf6a1cd8b5368aa16cd86dc2e27fb34588
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2024-02-28 21:37:21 +0100

    Install lexik/jwt-authentication-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
