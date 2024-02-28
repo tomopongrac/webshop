fa81e3dde6a8e83e52a650594235d6a45b5ef80b
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2024-02-28 18:30:10 +0100

    Install zenstruck/foundry

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     TomoPongrac\WebshopApiBundle\WebshopApiBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
