4441f9c13d880b9481787c45d1cd3552938a017d
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2024-02-29 20:04:21 +0100

    Create command for seeding tax categories

---
+++ tests/integration/Command/SeedTaxCategoriesCommandTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\integration\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+use TomoPongrac\WebshopApiBundle\Entity\TaxCategory;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class SeedTaxCategoriesCommandTest extends KernelTestCase
+{
+    use ResetDatabase, Factories;
+
+    /** @test */
+    public function seedCategoriesCommand(): void
+    {
+        $kernel = self::bootKernel();
+        $application = new Application(self::$kernel);
+
+        $entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $command = $application->find('webshop-api:seed-tax-categories');
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([]);
+
+        // check that the database was seeded with categories
+        $categories = $entityManager
+            ->getRepository(TaxCategory::class)
+            ->findAll();
+
+        $this->assertCount(5, $categories);
+    }
+}
