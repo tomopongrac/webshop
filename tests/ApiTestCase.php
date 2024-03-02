da460841fce6ea2c5aea66954f73f2819c149f15
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2024-03-02 20:23:48 +0100

    Create method for authenticating user in tests

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zenstruck\Browser\KernelBrowser;
 use Zenstruck\Browser\Test\HasBrowser;
 
 class ApiTestCase extends WebTestCase
 {
     use HasBrowser {
         browser as baseKernelBrowser;
+    }
+
+    protected function authenticateUserInBrowser(User $user): KernelBrowser
+    {
+        $tokenManager = static::getContainer()->get(JWTTokenManagerInterface::class);
+        $token = $tokenManager->create($user);
+
+        return $this->baseKernelBrowser()
+            ->setDefaultHttpOptions(['headers' => ['Authorization' => 'Bearer ' . $tokenManager->create($user)]]);
     }
 }
