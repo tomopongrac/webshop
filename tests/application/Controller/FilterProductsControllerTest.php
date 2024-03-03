94392a5165612a7f7d2baccad62f200b938a34e2
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2024-03-03 19:49:41 +0100

    Add filtering by prices from price list

--- tests/application/Controller/FilterProductsControllerTest.php
+++ tests/application/Controller/FilterProductsControllerTest.php
@@ -8,6 +8,8 @@ use App\Tests\ApiTestCase;
 use PHPUnit\Framework\Attributes\Test;
 use Symfony\Component\HttpFoundation\Response;
 use TomoPongrac\WebshopApiBundle\Factory\CategoryFactory;
+use TomoPongrac\WebshopApiBundle\Factory\PriceListFactory;
+use TomoPongrac\WebshopApiBundle\Factory\PriceListProductFactory;
 use TomoPongrac\WebshopApiBundle\Factory\ProductFactory;
 use TomoPongrac\WebshopApiBundle\Factory\TaxCategoryFactory;
 use Zenstruck\Browser\Json;
@@ -216,5 +218,84 @@ class FilterProductsControllerTest extends ApiTestCase
             ->assertMatches('total_pages', 1)
             ->assertMatches('total_results', 1)
             ->assertMatches('data[0].id', $product2->getId());
+    }
+
+    /** @test */
+    public function guestCanFilterProductsByPricesFromPriceList(): void
+    {
+        $taxCategory = TaxCategoryFactory::createOne()->object();
+        $category1 = CategoryFactory::createOne()->object();
+        $category2 = CategoryFactory::createOne()->object();
+        $categoryUnfiltered = CategoryFactory::createOne()->object();
+
+        $product1 = ProductFactory::new(
+            [
+                'name' => 'Product name',
+                'taxCategory' => $taxCategory,
+                'categories' => [$category1],
+                'price' => 1000,
+            ]
+        )->published()->create();
+
+        $product2 = ProductFactory::new(
+            [
+                'name' => 'Product name',
+                'taxCategory' => $taxCategory,
+                'categories' => [$category2],
+                'price' => 5000,
+            ]
+        )->published()->create();
+
+        $wrongProduct = ProductFactory::new(
+            [
+                'name' => 'name',
+                'taxCategory' => $taxCategory,
+                'categories' => [$categoryUnfiltered],
+                'price' => 3000,
+            ]
+        )->published()->create();
+
+        $priceList = PriceListFactory::createOne()->object();
+
+        PriceListProductFactory::createOne([
+            'priceList' => $priceList,
+            'product' => $product2,
+            'price' => 2000,
+        ]);
+
+        $request = [
+            'filters' => [
+                'name' => '',
+                'categories' => [],
+                'price' => [
+                    'min' => 1500,
+                    'max' => 2500,
+                ],
+            ],
+            'order' => [
+                'by' => 'name',
+                'direction' => 'asc',
+            ],
+            'pagination' => [
+                'page' => 1,
+                'limit' => 10,
+            ],
+        ];
+
+        $json = $this->baseKernelBrowser()
+            ->post(self::ENDPOINT_URL, [
+                'json' => $request,
+            ])
+            ->assertJson()
+            ->assertStatus(Response::HTTP_OK)
+            ->json();
+
+        $json->assertHas('current_page')
+            ->assertMatches('current_page', 1)
+            ->assertMatches('limit', 10)
+            ->assertMatches('total_pages', 1)
+            ->assertMatches('total_results', 1)
+            ->assertMatches('data[0].id', $product2->getId())
+            ->assertMatches('data[0].price.amount', '20.00');
     }
 }
